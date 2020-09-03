8b6a4509dc298df4ec09d3c219b0dddb92266249
Sui <admin@Mac-mini.local>
2020-09-03 15:50:17 -0400

    pushed function to component to bring back entered Goal

---
+++ components/GoalInput.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+const GoalInput = props => {
+    const [enteredGoal, setEnteredGoal] = useState('');
+
+     // set state from input text
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    };
+
+    return(
+        <View style={ styles.inputContainer }>
+        <TextInput 
+          placeholder="Course Goal" 
+          style={ styles.input }
+          onChangeText={goalInputHandler}
+          value={enteredGoal} />
+        <Button 
+          title="ADD"
+          onPress={() => props.onAddGoal(enteredGoal)} />
+          {/* can also write: onPress={props.onAddGoal.bind(this, enteredGoal)} */}
+      </View>
+    )
+};
+
+
+const styles = StyleSheet.create({
+    inputContainer: {
+      flexDirection: 'row', 
+      justifyContent: 'space-between'
+    },
+    input: {
+      borderColor: 'black', 
+      borderWidth: 1, 
+      padding: 10, 
+      width: '80%'
+    }
+  });
+
+export default GoalInput;
